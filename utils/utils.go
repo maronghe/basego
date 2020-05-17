be80857819e2156e43077d050120d7329378d953
maronghe <598806805@qq.com>
2020-05-17 13:28:51 +0800

    update utils

--- utils/utils.go
+++ utils/utils.go
@@ -6,6 +6,8 @@
 package utils
 
 import (
+	"bytes"
+	"encoding/binary"
 	"math/rand"
 	"time"
 )
@@ -30,4 +32,38 @@ func Fastrand(seed1, seed2 uint32) uint32 {
 	s1 ^= s1 << 17
 	s1 = s1 ^ s0 ^ s1>>7 ^ s0>>16
 	return s0 + s1
+}
+
+// IntToBytesBigEndian int to Big-Endian []byte
+func IntToBytesBigEndian(n int) []byte {
+	x := int32(n)
+	bytesBuffer := bytes.NewBuffer([]byte{})
+	binary.Write(bytesBuffer, binary.BigEndian, x)
+	return bytesBuffer.Bytes()
+}
+
+// IntToBytesLittleEndian int to Little-Endian []byte
+func IntToBytesLittleEndian(n int) []byte {
+	x := int32(n)
+	bytesBuffer := bytes.NewBuffer([]byte{})
+	binary.Write(bytesBuffer, binary.LittleEndian, x)
+	return bytesBuffer.Bytes()
+}
+
+// BytesToIntBigEndian Big-Endian []byte to int
+func BytesToIntBigEndian(b []byte) int {
+	bytesBuffer := bytes.NewBuffer(b)
+
+	var x int32
+	binary.Read(bytesBuffer, binary.BigEndian, &x)
+
+	return int(x)
+}
+
+// BytesToIntLittleEndian Little-Endian []byte to int
+func BytesToIntLittleEndian(b []byte) int {
+	bytesBuffer := bytes.NewBuffer(b)
+	var x int32
+	binary.Read(bytesBuffer, binary.LittleEndian, &x)
+	return int(x)
 }
--- utils/utils_test.go
+++ utils/utils_test.go
@@ -1,14 +1,33 @@
 package utils_test
 
 import (
+	"basego/utils"
 	"fmt"
 	"testing"
-
-	"basego/utils"
 )
 
-func TestRandom(t *testing.T) {
+func TestRanndom(t *testing.T) {
+	fmt.Printf("%s\n", utils.RandomString(4))
+}
 
-	fmt.Println(utils.RandomString(10))
+func BenchmarkRandom(b *testing.B) {
+	for i := 1; i <= 5; i++ {
+		b.Run(fmt.Sprintf("i-%d", i), func(b *testing.B) {
+			// b.ResetTimer()
+			fmt.Printf(" %d %s \n", i, utils.RandomString(4))
+		})
+	}
+}
+
+func TestBigLittleEndian(t *testing.T) {
+	var i int = 6
+	bByte := utils.IntToBytesBigEndian(i)
+	ni := utils.BytesToIntBigEndian(bByte)
+	fmt.Printf("%d %b \n", ni, bByte) //	6 [0 0 0 110] 大端
+
+	var j int = 6
+	lByte := utils.IntToBytesLittleEndian(j)
+	nj := utils.BytesToIntLittleEndian(lByte)
+	fmt.Printf("%d %b \n", nj, lByte) // 6 [110 0 0 0] 小端
 
 }
